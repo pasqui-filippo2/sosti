f07af6d89def7817ad0598f73bcf83108a9ad583
filippo <pasqui.filippo@franchettisalviani.net>
2025-02-26 10:15:31 +0100

    1° versione

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dammi una parola");
+string parola=Console.ReadLine();
+Console.WriteLine("dammi k1");
+int k1=Int32.Parse(Console.ReadLine());
+Console.WriteLine("dammi k2");
+int k2 = Int32.Parse(Console.ReadLine());
+
+string sostituzione(string parola,int chiave)
+{
+    char[] arr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i','j' ,'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w','x','y','z' };
+    string ris = "";
+    for(int i = 0; i < parola.Length; i++)
+    {
+        for(int j = 0; j < arr.Length; j++)
+        {
+            if (parola[i] == arr[j])
+            {
+                
+                
+                if (j+chiave >= arr.Length-1 )
+                {
+                  
+                    ris += arr[0 + chiave-(26-j)];
+                }
+                else
+                {
+                    ris += arr[j + chiave];
+                }
+                
+            }
+        }
+    }
+    return ris;
+}
+Console.WriteLine(sostituzione(parola,k1));
+
